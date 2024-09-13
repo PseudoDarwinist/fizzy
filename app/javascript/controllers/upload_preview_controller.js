fa4eb26eb5cf7e25bfacd9ff4908f0d4efe9c270
Jeffrey Hardy <jeff@37signals.com>
2024-09-13 19:50:33 -0400

    Remove unnecessary image-preview#clear
    Destroying an image happens asynchronously and requests are fast enough that
    we don't need to be optimistic. Uploads by contrast are slow, and benefit
    from immediate feedback.

--- app/javascript/controllers/upload_preview_controller.js
+++ app/javascript/controllers/upload_preview_controller.js
@@ -1,20 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { defaultImage: String }
-  static targets = [ "image", "input", "button" ]
+  static targets = [ "image", "input" ]
 
   previewImage() {
     const file = this.inputTarget.files[0]
 
     if (file) {
-      this.imageTarget.src = URL.createObjectURL(this.inputTarget.files[0]);
-      this.imageTarget.onload = () => { URL.revokeObjectURL(this.imageTarget.src) }
+      this.imageTarget.src = URL.createObjectURL(file)
+      this.imageTarget.onload = () => URL.revokeObjectURL(this.imageTarget.src)
     }
-  }
-
-  clear() {
-    this.imageTarget.src = this.defaultImageValue
-    this.buttonTarget.style.visibility = "hidden"
   }
 }
